3d9791590531eec13f80940103841a218460abf4
super <chengchao@yanju.cn>
2022-09-13 11:21:24 +0800

    init

---
+++ subgraphs/nft-market/mappings/utils/erc721.ts
@@ -0,0 +1,59 @@
+/* eslint-disable prefer-const */
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { IERC721 } from "../../generated/MetaSportsNFTMarket/IERC721";
+import { MetaSportsNFT } from "../../generated/MetaSportsNFTMarket/MetaSportsNFT";
+
+export function fetchName(collectionAddress: Address): string {
+  let contract = IERC721.bind(collectionAddress);
+
+  let nameResult = contract.try_name();
+  if (!nameResult.reverted) {
+    return nameResult.value;
+  }
+
+  return "unknown";
+}
+
+export function fetchSymbol(collectionAddress: Address): string {
+  let contract = IERC721.bind(collectionAddress);
+
+  let symbolResult = contract.try_symbol();
+  if (!symbolResult.reverted) {
+    return symbolResult.value;
+  }
+
+  return "unknown";
+}
+
+export function fetchTotalSupply(collectionAddress: Address): BigInt | null {
+  let contract = MetaSportsNFT.bind(collectionAddress);
+
+  let symbolResult = contract.try_totalSupply();
+  if (!symbolResult.reverted) {
+    return symbolResult.value;
+  }
+
+  return null;
+}
+
+export function fetchTokenURI(collectionAddress: Address, tokenId: BigInt): string | null {
+  let contract = MetaSportsNFT.bind(collectionAddress);
+
+  let tokenURIResult = contract.try_tokenURI(tokenId);
+  if (!tokenURIResult.reverted) {
+    return tokenURIResult.value;
+  }
+
+  return null;
+}
+
+export function fetchBunnyId(collectionAddress: Address, tokenId: BigInt): BigInt | null {
+  let contract = MetaSportsNFT.bind(collectionAddress);
+
+  let bunnyIdResult = contract.try_getBunnyId(tokenId);
+  if (!bunnyIdResult.reverted) {
+    return BigInt.fromI32(bunnyIdResult.value);
+  }
+
+  return null;
+}
